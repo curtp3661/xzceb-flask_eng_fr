6ff9ddc2f0993444a7c4b743b17d117bfa75014f
curtp3661 <curtp3661@gmail.com>
2022-03-25 07:01:25 +0000

    more changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,3 +1,6 @@
+'''
+python project - language translator
+'''
 import json
 import os
 from ibm_watson import LanguageTranslatorV3
@@ -10,17 +13,22 @@ apikey = os.environ['apikey']
 url = os.environ['url']
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
-    version='2018-05-01', 
-    authenticator=authenticator
-)
+    version='2018-05-01',
+    authenticator=authenticator)
 language_translator.set_service_url(url)
-
-def english_to_french(englishText):
-    translation = language_translator.translate(text=englishText, model_id='en-fr').get_result()
-    frenchText = translation['translations'][0]['translation']
-    return frenchText
-    
-def french_to_english(frenchText):
-    translation = language_translator.translate(text=frenchText, model_id='fr-en').get_result()
-    englishText = translation['translations'][0]['translation']
-    return englishText
+# code for english-french translation w/IBM Watson Instance
+def english_to_french(english_text):
+    '''
+    code for english to french
+    '''
+    translation = language_translator.translate(text=english_text, model_id='en-fr').get_result()
+    french_text = translation['translations'][0]['translation']
+    return french_text
+#code for english-french translation w/IBM Watson Instance
+def french_to_english(french_text):
+    '''
+    #code for french to english
+    '''
+    translation = language_translator.translate(text=french_text, model_id='fr-en').get_result()
+    english_text = translation['translations'][0]['translation']
+    return english_text
